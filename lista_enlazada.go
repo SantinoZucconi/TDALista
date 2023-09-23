42c229623ba9691c5f9fd475ab92871dfcce770a
SantinoZucconi <santinozucconi@gmail.com>
2023-09-23 14:27:05 -0300

    Corrigiendo archivos

--- lista_enlazada.go
+++ lista_enlazada.go
@@ -1,35 +1,89 @@
 package lista
 
-type Lista[T any] interface {
-	//
-	EstaVacia() bool
-	//
-	InsertarPrimero(T)
-	//
-	InsertarUltimo(T)
-	//
-	BorrarPrimero() T
-	//
-	VerPrimero() T
-	//
-	VerUltimo() T
-	//
-	Largo() int
-	//
-	Iterar(visitar func(T) bool)
-	//
-	Iterador() IteradorLista[T]
+type ListaEnlazada[T any] struct {
+	inicio *nodoLista[T]
+	fin    *nodoLista[T]
 }
 
-type IteradorLista[T any] interface {
-	//
-	VerActual() T
-	//
-	HaySiguiente() bool
-	//
-	Siguiente()
-	//
-	Insertar(T)
-	//
-	Borrar() T
+type Iterador[T any] struct {
+	anterior *nodoLista[T]
+	actual   *nodoLista[T]
+}
+
+type nodoLista[T any] struct {
+	dato      T
+	siguiente *nodoLista[T]
+}
+
+func CrearListaEnlazada[T any]() Lista[T] {
+	return &ListaEnlazada[T]{}
+}
+
+func (l *ListaEnlazada[T]) EstaVacia() bool {
+	return l.inicio == nil
+}
+
+func (l *ListaEnlazada[T]) InsertarPrimero(dato T) {
+
+}
+
+func (l *ListaEnlazada[T]) InsertarUltimo(dato T) {
+
+}
+
+func (l *ListaEnlazada[T]) BorrarPrimero() T {
+}
+
+func (l *ListaEnlazada[T]) VerPrimero() T {
+}
+
+func (l *ListaEnlazada[T]) VerUltimo() T {
+}
+
+func (l *ListaEnlazada[T]) Largo() int {
+}
+
+func (l *ListaEnlazada[T]) Iterar(visitar func(T) bool) {
+}
+
+//Iterador
+
+func (l *ListaEnlazada[T]) Iterador() IteradorLista[T] {
+	return &Iterador[T]{}
+}
+
+func (i *Iterador[T]) VerActual() T {
+	if i.actual != nil {
+		return i.actual.dato
+	} else {
+		panic("El iterador termino de iterar")
+	}
+}
+
+func (i *Iterador[T]) HaySiguiente() bool {
+	return i.actual != nil
+}
+
+func (i *Iterador[T]) Siguiente() {
+	if i.actual != nil {
+		i.anterior = i.actual
+		i.actual = i.actual.siguiente
+	} else {
+		panic("El iterador termino de iterar")
+	}
+}
+
+func (i *Iterador[T]) Insertar(dato T) {
+	nuevo_nodo := &nodoLista[T]{dato: dato, siguiente: i.actual}
+	i.anterior.siguiente = nuevo_nodo
+	i.anterior = nuevo_nodo
+}
+
+func (i *Iterador[T]) Borrar() T {
+	if i.actual != nil {
+		i.anterior.siguiente = i.actual.siguiente
+		return i.actual.dato
+	} else {
+		panic("El iterador termino de iterar")
+	}
 }
